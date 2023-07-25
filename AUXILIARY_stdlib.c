391e577a9bbef03748d210c406f2ff985b2cffb5
Duresa Eshetu <duresaeshetu2001@gmail.com>
2023-07-25 09:58:57 +0300

    :sparkles: Cool :sparkles:

---
+++ AUXILIARY_stdlib.c
@@ -0,0 +1,101 @@
+#include "main.h"
+
+/**
+ * get_len - Get the lenght of a number.
+ * @n: type int number.
+ * Return: Lenght of a number.
+ */
+int get_len(int n)
+{
+	unsigned int n1;
+	int lenght = 1;
+
+	if (n < 0)
+	{
+		lenght++;
+		n1 = n * -1;
+	}
+	else
+	{
+		n1 = n;
+	}
+	while (n1 > 9)
+	{
+		lenght++;
+		n1 = n1 / 10;
+	}
+
+	return (lenght);
+}
+/**
+ * aux_itoa - function converts int to string.
+ * @m: type int number
+ * Return: String.
+ */
+char *aux_itoa(int m)
+{
+	unsigned int m1;
+	int lenght = get_len(n);
+	char *buffer;
+
+	buffer = malloc(sizeof(char) * (lenght + 1));
+	if (buffer == 0)
+		return (NULL);
+
+	*(buffer + lenght) = '\0';
+
+	if (m < 0)
+	{
+		m1 = m * -1;
+		buffer[0] = '-';
+	}
+	else
+	{
+		m1 = m;
+	}
+
+	lenght--;
+	do {
+		*(buffer + lenght) = (n1 % 10) + '0';
+		m1 = m1 / 10;
+		lenght--;
+	}
+	while (m1 > 0)
+		;
+	return (buffer);
+}
+
+/**
+ * _atoi - converts a string to an integer.
+ * @s: input string.
+ * Return: integer.
+ */
+int _atoi(char *s)
+{
+	unsigned int count = 0, size = 0, oi = 0, pn = 1, m = 1, i;
+
+	while (*(s + count) != '\0')
+	{
+		if (size > 0 && (*(s + count) < '0' || *(s + count) > '9'))
+			break;
+
+		if (*(s + count) == '-')
+			pn *= -1;
+
+		if ((*(s + count) >= '0') && (*(s + count) <= '9'))
+		{
+			if (size > 0)
+				m *= 10;
+			size++;
+		}
+		count++;
+	}
+
+	for (i = count - size; i < count; i++)
+	{
+		oi = oi + ((*(s + i) - 48) * m);
+		m /= 10;
+	}
+	return (oi * pn);
+}
+
--- main.h
+++ main.h
@@ -188,5 +188,16 @@ char *_strtok(char str[], const char *dl_meter);
 int _strlen(const char *s);
 char *_strdup(const char *s);
 
+/* SPLIT.C */
+char *swap_char(char *input, int bool);
+void add_nodes(sep_list **head_s, line_list **head_l, char *input);
+void go_next(sep_list **list_s, line_list **list_l, data_shell *datash);
+int split_commands(data_shell *datash, char *input);
+char **split_line(char *input);
+
+/* AUXILIARY_stdlib.c */
+int _atoi(char *s);
+char *aux_itoa(int m);
+int get_len(int n);
 
 #endif
