d5ad109c588590370954638c7f8b47ad321f9998
Duresa Eshetu <duresaeshetu2001@gmail.com>
2023-07-26 16:40:12 +0300

    :sparkles: Cool :sparkles:

--- AUX_LISTS.c
+++ AUX_LISTS.c
@@ -1,10 +1,42 @@
 #include "main.h"
 
 /**
+ * add_sep_node_end - adds a separator found at the end
+ * of a sep_list.
+ * @head: head of the linked list.
+ * @sep: separator found (; | &).
+ * Return: address of the head.
+ */
+sep_list *add_sep_node_end(sep_list **head, char sep)
+{
+	sep_list *new, *temp;
+
+	new = malloc(sizeof(sep_list));
+	if (new == NULL)
+		return (NULL);
+
+	new->separator = sep;
+	new->next = NULL;
+	temp = *head;
+
+	if (temp == NULL)
+	{
+		*head = new;
+	}
+	else
+	{
+		while (temp->next != NULL)
+			temp = temp->next;
+		temp->next = new;
+	}
+
+	return (*head);
+}
+
+/**
  * free_sep_list - frees a sep_list
  * @head: head of the linked list.
- *
- * Return: it have no return.
+ * Return: no return.
  */
 void free_sep_list(sep_list **head)
 {
@@ -24,34 +56,11 @@ void free_sep_list(sep_list **head)
 }
 
 /**
- * free_line_list - frees a line_list
- * @head: head of linked list.
- *
- * Return:  it have no return.
- */
-void free_line_list(line_list **head)
-{
-	line_list *temp;
-	line_list *curr;
-
-	if (head != NULL)
-	{
-		curr = *head;
-		while ((temp = curr) != NULL)
-		{
-			curr = curr->next;
-			free(temp);
-		}
-		*head = NULL;
-	}
-}
-
-/**
- * add_line_node_end - the function adds a command(cmd) line at the end of a line_list.
+ * add_line_node_end - adds a command line at the end
+ * of a line_list.
  * @head: head of the linked list.
- * @line: command(cmd) line.
- *
- * Return: should return address of the head.
+ * @line: command line.
+ * Return: address of the head.
  */
 line_list *add_line_node_end(line_list **head, char *line)
 {
@@ -78,3 +87,26 @@ line_list *add_line_node_end(line_list **head, char *line)
 
 	return (*head);
 }
+
+/**
+ * free_line_list - frees a line_list
+ * @head: head of the linked list.
+ * Return: no return.
+ */
+void free_line_list(line_list **head)
+{
+	line_list *temp;
+	line_list *curr;
+
+	if (head != NULL)
+	{
+		curr = *head;
+		while ((temp = curr) != NULL)
+		{
+			curr = curr->next;
+			free(temp);
+		}
+		*head = NULL;
+	}
+}
+
