6b66ab7b71279abec236b5b5ea6b86e2697b6bd2
Duresa Eshetu <duresaeshetu2001@gmail.com>
2023-07-29 03:43:56 +0300

    :sparkles:

--- AUX_LISTS.c
+++ AUX_LISTS.c
@@ -1,111 +1,86 @@
 #include "main.h"
 
 /**
- * add_sep_node_end - adds a separator found at the end
- * of a sep_list.
+ * Function to add a separate node at the end of the separator list
  * @head: head of the linked list.
  * @sep: separator found (; | &).
- * Return: address of the head.
+ * Return: address of the new node.
  */
-sep_list *add_sep_node_end(sep_list **head, char sep)
-{
-	sep_list *new, *temp;
+sep_list *add_sep_node_end(sep_list **head, char sep) {
+    sep_list *new_node = (sep_list *)malloc(sizeof(sep_list));
+    new_node->sep = sep;
+    new_node->next = NULL;
 
-	new = malloc(sizeof(sep_list));
-	if (new == NULL)
-		return (NULL);
+    if (*head == NULL) {
+        *head = new_node;
+    } else {
+        sep_list *temp = *head;
+        while (temp->next != NULL) {
+            temp = temp->next;
+        }
+        temp->next = new_node;
+    }
 
-	new->separator = sep;
-	new->next = NULL;
-	temp = *head;
-
-	if (temp == NULL)
-	{
-		*head = new;
-	}
-	else
-	{
-		while (temp->next != NULL)
-			temp = temp->next;
-		temp->next = new;
-	}
-
-	return (*head);
+    return new_node;
 }
 
 /**
- * free_sep_list - frees a sep_list
+ * free_sep_list - free the memory allocated for the separator list
  * @head: head of the linked list.
  * Return: no return.
  */
-void free_sep_list(sep_list **head)
-{
-	sep_list *temp;
-	sep_list *curr;
+void free_sep_list(sep_list **head) {
+    sep_list *current = *head;
+    sep_list *next;
 
-	if (head != NULL)
-	{
-		curr = *head;
-		while ((temp = curr) != NULL)
-		{
-			curr = curr->next;
-			free(temp);
-		}
-		*head = NULL;
-	}
+    while (current != NULL) {
+        next = current->next;
+        free(current);
+        current = next;
+    }
+
+    *head = NULL;
 }
 
 /**
- * add_line_node_end - adds a command line at the end
- * of a line_list.
+ * add_line_node_end - add a line node at the end of the line list
  * @head: head of the linked list.
  * @line: command line.
  * Return: address of the head.
  */
-line_list *add_line_node_end(line_list **head, char *line)
-{
-	line_list *new, *temp;
+line_list *add_line_node_end(line_list **head, char *line) {
+    line_list *new_node = (line_list *)malloc(sizeof(line_list));
+    new_node->line = line;
+    new_node->next = NULL;
 
-	new = malloc(sizeof(line_list));
-	if (new == NULL)
-		return (NULL);
+    if (*head == NULL) {
+        *head = new_node;
+    } else {
+        line_list *temp = *head;
+        while (temp->next != NULL) {
+            temp = temp->next;
+        }
+        temp->next = new_node;
+    }
 
-	new->line = line;
-	new->next = NULL;
-	temp = *head;
-
-	if (temp == NULL)
-	{
-		*head = new;
-	}
-	else
-	{
-		while (temp->next != NULL)
-			temp = temp->next;
-		temp->next = new;
-	}
-
-	return (*head);
+    return new_node;
 }
 
 /**
- * free_line_list - frees a line_list
+ * Function to free the memory allocated for the line list
  * @head: head of the linked list.
  * Return: no return.
  */
-void free_line_list(line_list **head)
-{
-	line_list *temp;
-	line_list *curr;
+void free_line_list(line_list **head) {
+    line_list *current = *head;
+    line_list *next;
 
-	if (head != NULL)
-	{
-		curr = *head;
-		while ((temp = curr) != NULL)
-		{
-			curr = curr->next;
-			free(temp);
-		}
-		*head = NULL;
-	}
+    while (current != NULL) {
+        next = current->next;
+        free(current->line);
+        free(current);
+        current = next;
+    }
+
+    *head = NULL;
 }
