456e45884cbd60e3736455de5f9d0e9ea4f6649e
Duresa Eshetu <duresaeshetu2001@gmail.com>
2023-07-29 04:12:30 +0300

    :sparkles:

--- aux_str2.c
+++ aux_str2.c
@@ -5,32 +5,31 @@
  * @s: Type char pointer str
  * Return: duplicated str
  */
-char *_strdup(const char *s)
-{
-	char *new;
-	size_t len;
-
-	len = _strlen(s);
-	new = malloc(sizeof(char) * (len + 1));
-	if (new == NULL)
-		return (NULL);
-	_memcpy(new, s, len + 1);
-	return (new);
+char *_strdup(const char *s) {
+    int length = strlen(s);
+    char *dup = malloc(length + 1);
+    
+    if (dup != NULL) {
+        strcpy(dup, s);
+    }
+    
+    return dup;
 }
 
 /**
- * _strlen - Returns the lenght of a string.
+ * _strlen - calculate the lenght of a string.
  * @s: Type char pointer
  * Return: Always 0.
  */
-int _strlen(const char *s)
-{
-	int len;
-
-	for (len = 0; s[len] != 0; len++)
-	{
-	}
-	return (len);
+int _strlen(const char *s) {
+    int length = 0;
+    
+    while (*s != '\0') {
+        length++;
+        s++;
+    }
+    
+    return length;
 }
 
 /**
@@ -40,24 +39,18 @@ int _strlen(const char *s)
  *
  * Return: 1 if are equals, 0 if not.
  */
-int cmp_chars(char str[], const char *delim)
-{
-	unsigned int i, j, k;
-
-	for (i = 0, k = 0; str[i]; i++)
-	{
-		for (j = 0; delim[j]; j++)
-		{
-			if (str[i] == delim[j])
-			{
-				k++;
-				break;
-			}
-		}
-	}
-	if (i == k)
-		return (1);
-	return (0);
+int cmp_chars(char str[], const char *delim) {
+    int matched = 0;
+    
+    while (*str != '\0') {
+        if (strchr(delim, *str) != NULL) {
+            matched++;
+        }
+        
+        str++;
+    }
+    
+    return matched;
 }
 
 /**
@@ -67,63 +60,43 @@ int cmp_chars(char str[], const char *delim)
  *
  * Return: string splited.
  */
-char *_strtok(char str[], const char *delim)
-{
-	static char *splitted, *str_end;
-	char *str_start;
-	unsigned int i, bool;
-
-	if (str != NULL)
-	{
-		if (cmp_chars(str, delim))
-			return (NULL);
-		splitted = str; /*Store first address*/
-		i = _strlen(str);
-		str_end = &str[i]; /*Store last address*/
-	}
-	str_start = splitted;
-	if (str_start == str_end) /*Reaching the end*/
-		return (NULL);
-
-	for (bool = 0; *splitted; splitted++)
-	{
-		/*Breaking loop finding the next token*/
-		if (splitted != str_start)
-			if (*splitted && *(splitted - 1) == '\0')
-				break;
-		/*Replacing delimiter for null char*/
-		for (i = 0; delim[i]; i++)
-		{
-			if (*splitted == delim[i])
-			{
-				*splitted = '\0';
-				if (splitted == str_start)
-					str_start++;
-				break;
-			}
-		}
-		if (bool == 0 && *splitted) /*Str != Delim*/
-			bool = 1;
-	}
-	if (bool == 0) /*Str == Delim*/
-		return (NULL);
-	return (str_start);
+char *_strtok(char str[], const char *delim) {
+    static char *next = NULL;
+    
+    if (str != NULL) {
+        next = str;
+    }
+    
+    if (next == NULL) {
+        return NULL;
+    }
+    
+    char *token = next;
+    char *delimiter = strpbrk(next, delim);
+    
+    if (delimiter != NULL) {
+        *delimiter = '\0';
+        next = delimiter + 1;
+    } else {
+        next = NULL;
+    }
+    
+    return token;
 }
 
 /**
- * _isdigit - defines if string passed is a number
- *
- * @s: input string
+ * _isdigit - check if a string represents a digit
+ *@s: input string
  * Return: 1 if string is a number. 0 in other case.
  */
-int _isdigit(const char *s)
-{
-	unsigned int i;
-
-	for (i = 0; s[i]; i++)
-	{
-		if (s[i] < 48 || s[i] > 57)
-			return (0);
-	}
-	return (1);
+int _isdigit(const char *s) {
+    while (*s != '\0') {
+        if (!isdigit(*s)) {
+            return 0;
+        }
+        
+        s++;
+    }
+    
+    return 1;
 }
