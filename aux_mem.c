7af2589eee186d48f02dc66965e2b6f765b758da
Duresa Eshetu <duresaeshetu2001@gmail.com>
2023-07-29 04:04:40 +0300

    :sparkles:

--- aux_mem.c
+++ aux_mem.c
@@ -10,16 +10,15 @@
  */
 void _memcpy(void *newptr, const void *ptr, unsigned int size)
 {
-	char *char_ptr = (char *)ptr;
-	char *char_newptr = (char *)newptr;
-	unsigned int i;
-
-	for (i = 0; i < size; i++)
-		char_newptr[i] = char_ptr[i];
+    char *d = (char *)newptr;
+    const char *s = (const char *)ptr;
+    
+    for (unsigned int i = 0; i < size; i++) {
+        d[i] = s[i];
+    }
 }
-
 /**
- * _realloc - reallocates a memory block.
+ * _realloc - dynamically reallocates a memory block.
  * @ptr: pointer to the memory previously allocated.
  * @old_size: size, in bytes, of the allocated space of ptr.
  * @new_size: new size, in bytes, of the new memory block.
@@ -28,33 +27,30 @@ void _memcpy(void *newptr, const void *ptr, unsigned int size)
  * if new_size == old_size, returns ptr without changes.
  * if malloc fails, returns NULL.
  */
-void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
-{
-	void *newptr;
-
-	if (ptr == NULL)
-		return (malloc(new_size));
-
-	if (new_size == 0)
-	{
-		free(ptr);
-		return (NULL);
-	}
-
-	if (new_size == old_size)
-		return (ptr);
-
-	newptr = malloc(new_size);
-	if (newptr == NULL)
-		return (NULL);
-
-	if (new_size < old_size)
-		_memcpy(newptr, ptr, new_size);
-	else
-		_memcpy(newptr, ptr, old_size);
-
-	free(ptr);
-	return (newptr);
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size) {
+    if (new_size == 0) {
+        free(ptr);
+        return NULL;
+    }
+    
+    if (ptr == NULL) {
+        return malloc(new_size);
+    }
+    
+    void *new_ptr = malloc(new_size);
+    if (new_ptr == NULL) {
+        return NULL;
+    }
+    
+    unsigned int copy_size = old_size;
+    if (new_size < old_size) {
+        copy_size = new_size;
+    }
+    
+    _memcpy(new_ptr, ptr, copy_size);
+    free(ptr);
+    
+    return new_ptr;
 }
 
 /**
@@ -67,25 +63,6 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
  * if new_size == old_size, returns ptr without changes.
  * if malloc fails, returns NULL.
  */
-char **_reallocdp(char **ptr, unsigned int old_size, unsigned int new_size)
-{
-	char **newptr;
-	unsigned int i;
-
-	if (ptr == NULL)
-		return (malloc(sizeof(char *) * new_size));
-
-	if (new_size == old_size)
-		return (ptr);
-
-	newptr = malloc(sizeof(char *) * new_size);
-	if (newptr == NULL)
-		return (NULL);
-
-	for (i = 0; i < old_size; i++)
-		newptr[i] = ptr[i];
-
-	free(ptr);
-
-	return (newptr);
+char **_reallocdp(char **ptr, unsigned int old_size, unsigned int new_size) {
+    return (char **)_realloc(ptr, old_size * sizeof(char *), new_size * sizeof(char *));
 }
